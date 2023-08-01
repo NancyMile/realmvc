bca47647a9061e64c75aea673b5a2f9a937d4287
nancy <smithnancy@outlook.com>
2023-08-01 14:01:18 +1000

    controller for sellers and routes created

---
+++ controllers/SellerController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Controllers;
+use MVC\Router;
+use Model\Seller;
+
+class SellerController{
+    public static function create(){
+        echo "CREATE";
+    }
+
+    public static function update(){
+       echo "UPDATE"; 
+    }
+
+    public static function delete(){
+        echo "DELETE";   
+    }
+}
